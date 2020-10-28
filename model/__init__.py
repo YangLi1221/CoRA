03f54f9dbfc0a392b82c431377244f8246b1911a
UsamiLee <usamiharu.sheep@gmail.com>
2020-10-28 16:22:49 +1100

    CoRA_modified

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .model_first import CoRA
+from .model_CoRA import CoRA
 
 __all__ = [
     'CoRA'
